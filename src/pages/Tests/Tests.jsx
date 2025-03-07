a1e2db5e785aa2a3a101ee47dc074313a65cf3a4
MDZAHIDHASAN2024 <zahidbinanwar3@gmail.com>
2025-03-07 11:37:36 +0600

    form hande 01

--- src/pages/Tests/Tests.jsx
+++ src/pages/Tests/Tests.jsx
@@ -1,26 +1,65 @@
 import React, { useState } from 'react';
 
 const Test = () => {
-  const handleChild = (e) => {
-    e.stopPropagation();
-    console.log(e);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
   };
-  const handleParent = (e) => {
-    console.log(e);
+  const hangeEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userInfo = {
+      name,
+      email,
+      password,
+    };
+    console.log(userInfo);
   };
 
   return (
     <div>
-      <div className="border" onClick={handleParent}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero alias
-          voluptate iste dicta pariatur laudantium rem beatae exercitationem
-          cupiditate nihil.
-        </p>
-        <div onClick={handleChild} className="border">
-          <button>click</button>
+      <form action="" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name"> Name: </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+          />
         </div>
-      </div>
+        <div className="mb-3">
+          <label htmlFor="email"> Email: </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={hangeEmailChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password"> Password: </label>
+          <label htmlFor="password"> Password: </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
