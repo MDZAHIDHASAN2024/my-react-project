1a81dfda35064bbf0e196c3a44407033dce070a6
MDZAHIDHASAN2024 <zahidbinanwar3@gmail.com>
2025-03-07 10:01:30 +0600

    useState

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import Home from '../pages/Home';
+import React, { useState } from 'react';
+import Tests from '../pages/Tests/Tests';
 
 const Index = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
   return (
     <div>
-      <Home />
+      <h2>{count}</h2>
+      <button onClick={handleIncrement} disabled={count === 10 ? true : false}>
+        +
+      </button>
+      <button onClick={handleDecrement} disabled={count === 0 ? true : false}>
+        -
+      </button>
     </div>
   );
 };
