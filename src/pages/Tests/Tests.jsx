4372ba0ce4c12ace31dde74ea1b4c60507107529
MDZAHIDHASAN2024 <zahidbinanwar3@gmail.com>
2025-03-08 07:19:09 +0600

    form handle use formik & yup

--- src/pages/Tests/Tests.jsx
+++ src/pages/Tests/Tests.jsx
@@ -1,21 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useFormik } from 'formik';
+import * as yup from 'yup';
 
-const Test = () => {
+const Tests = () => {
+  // use formik here
   const formik = useFormik({
     initialValues: {
       name: '',
+      email: '',
+      password: '',
     },
-    onSubmit: (values, { resetForm }) => {
+    onSubmit: (values) => {
       console.log(values);
-      resetForm({ values: '' });
     },
+
+    // use yup here
+    validationSchema: yup.object({
+      name: yup
+        .string()
+        .min(2, 'Name have must atleast 2 charachers')
+        .required(),
+      email: yup.string().email().required(),
+      password: yup
+        .string()
+        .min(6, 'Name have must atleast 6 charachers')
+        .required(),
+    }),
   });
+
+  // formik error handle here
+  const renderNameError = formik.touched.name && formik.errors.name && (
+    <span>{formik.errors.name}</span>
+  );
+  const renderEmailError = formik.touched.email && formik.errors.email && (
+    <span>{formik.errors.email}</span>
+  );
+  const renderPasswordError = formik.touched.password &&
+    formik.errors.password && <span>{formik.errors.password}</span>;
+
   return (
     <div>
       <form action="" onSubmit={formik.handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="name">Name:</label>
+          <label htmlFor="name">Name</label>
           <input
             type="text"
             name="name"
@@ -24,10 +51,33 @@ const Test = () => {
             onChange={formik.handleChange}
           />
         </div>
+        <div className="text-danger">{renderNameError}</div>
+        <div className="mb-3">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div className="text-danger">{renderEmailError}</div>
+        <div className="mb-3">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div className="text-danger">{renderPasswordError}</div>
         <button type="submit">Submit</button>
       </form>
     </div>
   );
 };
 
-export default Test;
+export default Tests;
